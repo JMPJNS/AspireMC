104916754b1bb4b7ba8fa28868e5668582cc9de9
Jonas <jonas@jmp.blue>
2025-07-25 19:41:59 +0200

    add a test server to the apphost

--- AspireMC.AppHost/AppHost.cs
+++ AspireMC.AppHost/AppHost.cs
@@ -1,7 +1,14 @@
 using AspireMC;
+using AspireMC.Config;
 
 var builder = DistributedApplication.CreateBuilder(args);
 
-
+builder.AddMinecraft("test-server", new MinecraftConfigBuilder()
+    .WithPort(25568)
+    .WithRam("8G")
+    .WithModrinthModpack("fabulously-optimized")
+    .AcceptEula()
+    .Build()
+);
 
 builder.Build().Run();
